514ef65ddca21a35eea5fe68ad0c1d96a09d79c2
aya <houtaitane0716@gmail.com>
2023-03-03 01:45:06 +0800

    add baseurl for github-pages

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  /** If you want to deploy site on github pages, you need to set base-url.
+   *  Learn more detail: https://vitejs.dev/guide/static-deploy.html#github-pages
+   */
+  base: '/dictionary-web-app/'
 })
